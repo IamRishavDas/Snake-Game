919c4ac49a95bd36c194c6d2c69551bd0cb3915e
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2024-02-08 23:10:20 +0530

    initial java file uploads

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+    MainFrame(){
+        this.setSize(700,700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
---
+++ src/MainPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainPanel extends JPanel implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
